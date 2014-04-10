652eccd72f64a920ed1cd8b6d54e9bd6b7684111
Benny Neugebauer <bn@bennyn.de>
2014-04-10 18:51:33 +0200

    Added tool to compare the syntax of different languages.

--- einzeiler/js/main.js
+++ einzeiler/js/main.js
@@ -1,27 +1,32 @@
 $(window).load(function() {
-  // Input & Output handling
-  $input = $('#input');
-  $output = $('#output');
-  $clipboard = $('#clipboard');
+	// Input & Output handling
+	$input = $('#input');
+	$output = $('#output');
+	$clipboard = $('#clipboard');
 
-  $input.on("focusout", function() {
-    var text = $(this).val();
-    var stripped = text.replace(/(\r\n|\n|\r)/gm, "");
-    $output.val(stripped);
-  });
+	$input.on("focusout", function() {
+		var text = $(this).val();
+		var stripped = text.replace(/(\r\n|\n|\r)/gm, "");
+		// TODO: Reduce white space
+		stripped = stripped.replace(/\s+/g, ' ');
+		$output.val(stripped);
+	});
 
-  // Copy to clipboard
-  ZeroClipboard.config({
-    moviePath: "./libs/zeroclipboard/1.3.4/ZeroClipboard.swf"
-  });
+	// Copy to clipboard
+	ZeroClipboard.config({
+		moviePath: "./libs/zeroclipboard/1.3.4/ZeroClipboard.swf"
+	});
 
-  var client = new ZeroClipboard($clipboard);
-  client.on("load", function(client) {
-    client.on("complete", function(client, args) {
-      $.notify("Text copied to clipboard.", "success", {
-        clickToHide: false,
-        autoHide: true
-      });
-    });
-  });
+	var client = new ZeroClipboard($clipboard);
+	client.on("load", function(client) {
+		client.on("complete", function(client, args) {
+			$.notify("Text copied to clipboard.", "success", {
+				autoHide: true,
+				clickToHide: false,
+				// TODO: Positioning does not work
+				elementPosition: 'top left',
+				globalPosition: 'top left'
+			});
+		});
+	});
 });
